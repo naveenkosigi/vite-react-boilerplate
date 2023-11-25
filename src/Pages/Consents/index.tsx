742bb857be52319631762cfbcbd6522fc61c075a
naveenkosigi <58993566+naveenkosigi@users.noreply.github.com>
2023-11-25 15:16:07 +0530

    Consent Page Init

--- src/Pages/Consents/index.tsx
+++ src/Pages/Consents/index.tsx
@@ -1,14 +1,14 @@
 import { useTranslation } from "react-i18next";
+import classes from "./consents.module.scss";
 
 const Consents = () => {
+  const { i18n, t } = useTranslation();
 
-    const {i18n , t} = useTranslation();
-    
-    return(
-        <>
-            {t("consents.demo")}
-        </>
-    )
-}
+  return (
+    <div className={classes["consent-container"]}>
+      <div className={classes["consent-content"]}>{t("consents.demo")}</div>
+    </div>
+  );
+};
 
 export default Consents;
